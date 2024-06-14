2b741fc8a92ea223edec207e58a3b3b25b2c550c
marko <marko@marko.com>
2024-06-14 18:16:54 +0200

    adding not-found and loading pages

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,10 @@
+'use client';
 import NewsList, { NewsItemProps, NewsProps } from "@/components/news/NewsList";
 import { DUMMY_NEWS } from "@/dummy_news";
-import React from "react";
-
-
+import React, { useEffect } from "react";
 
 export default function NewsPage() {
 
-
+ 
   return <NewsList news={DUMMY_NEWS} />;
 }
